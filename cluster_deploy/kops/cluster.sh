3c41142e71100e25bd615163deab00be13c03aa5
NimbusScale <joe@nimbusscale.com>
2018-05-23 17:41:34 -0700

    webservcie

--- cluster_deploy/kops/cluster.sh
+++ cluster_deploy/kops/cluster.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-export NAME=single.k8s.nimbusscale.com
+export NAME=lab.k8s.nimbusscale.com
 export KOPS_STATE_STORE=s3://bct-jjk3-kops
 export AWS_SDK_LOAD_CONFIG=1
 
 kops create cluster \
-    --node-count 1 \
-    --zones us-west-2a \
+    --node-count 2 \
+    --zones "us-west-2a,us-west-2b" \
     --master-zones us-west-2a \
     --node-size t2.small \
     --master-size t2.medium \
     --topology public \
     --networking canal \
-    --yes
+    --yes \
     --name ${NAME}
